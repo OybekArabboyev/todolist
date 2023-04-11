ee4d2ef63768328463c2303b708a69e9316b4867
OybekArabboyev <garatoretto@gmail.com>
2023-04-11 14:34:22 +0500

    create todo

---
+++ app.js
@@ -0,0 +1,38 @@
+const input = document.querySelector('#input')
+const btn = document.querySelector('#btn')
+const result = document.querySelector('#result')
+const total = document.querySelector('#total')
+let i = 0
+
+btn.addEventListener("click", (e) => {
+    // result.innerHTML += `<li>${input.value}</li>`
+    if(input.value === '') return
+    createDeleteElement(input.value)
+    input.value = ''
+})
+    /* todo'ni yaratish */
+function createDeleteElement(value) {
+    i++
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+
+    li.className = 'li'
+    li.textContent = value
+
+    btn.className = 'btn'
+    li.appendChild(btn)
+
+    /* todo'ni o`chirish */
+    btn.addEventListener("click", (e)=>{
+        result.removeChild(li)
+        i--
+    })
+    li.addEventListener("click", (e)=>{
+        li.classList.toggle('li-active')
+        btn.classList.toggle('btn-active')
+    })
+
+    
+
+    result.appendChild(li)
+}
